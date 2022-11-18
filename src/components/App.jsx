6c3893983429acb6590f1b5f9186653f36d71008
Aleksandr Tatarenko <aleksandrcrimea96@gmail.com>
2022-11-19 00:44:43 +0200

    hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
+import { ContactForm } from 'components/ContactForm/ContactForm';
+import { Contacts } from "components/Contacts/Contacts";
+import { Title } from "components/Contacts/Contacts.styled";
+import { Filter } from 'components/Filter/Filter';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+
+   return (<div>
+      <Title>Phonebook</Title>
+      <ContactForm />
+      <Title>Contacts</Title>
+      <Filter />
+      <Contacts />
     </div>
-  );
-};
+   )
+}
